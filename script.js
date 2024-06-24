40a9124c3d7085af9dcdc5fd5c6a922b336ad87f
Augusto2343 <augusto2343@gmail.com>
2024-06-24 13:26:40 -0300

    Subiendo tpsConcesionaria V2.0

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
+const slides = document.querySelectorAll('.slide');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
 
-Swal.fire("SweetAlert2 is working!");
+let currentSlide = 0;
+
+const showSlide = () => {
+    slides.forEach((slide, index) => {
+        slide.classList.remove('active');
+        if (index === currentSlide) {
+            slide.classList.add('active');
+        }
+    });
+};
+
+const nextSlide = () => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide();
+};
+
+const prevSlide = () => {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide();
+};
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
